c2f766256a8b57bfc7f704f5f0bce870aece966c
Hariprasath M <149574480+Hariprasath8064@users.noreply.github.com>
2024-10-06 15:20:01 +0530

    Update file 10/6/2024, 3:19:52 PM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,34 +1,31 @@
-public int coinChange(int[] coins, int amount) {
+class Solution {
+    public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][] dp = new int[n+1][amount+1];
-        
-        for (int i = 0; i < n+1; i++) {
-            for (int j = 0; j < amount+1; j++) {
-                if (i == 0) {
-                    dp[i][j] = Integer.MAX_VALUE - 1;
-                }
-                
-                if (j == 0) {
-                    dp[i][j] = 0;
-                }
-                
-                if (j % coins[0] == 0) {
-                    dp[i][j] = j / coins[0];
+        int dp[][] = new int[n + 1][amount + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0; 
+        }
+        for (int j = 1; j <= amount; j++) {
+            dp[0][j] = Integer.MAX_VALUE; 
+        }
+
+      
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j]; 
                 } else {
-                    dp[i][j] = Integer.MAX_VALUE - 1;
+                    
+                    if (dp[i][j - coins[i - 1]] != Integer.MAX_VALUE) {
+                        dp[i][j] = Math.min(dp[i][j - coins[i - 1]] + 1, dp[i - 1][j]);
+                    } else {
+                        dp[i][j] = dp[i - 1][j]; 
+                    }
                 }
             }
         }
-        
-        for (int i = 1; i < n+1; i++) {
-            for (int j = 1; j < amount + 1; j++) {
-                if (coins[i-1] <= j) {
-                    dp[i][j] = Math.min(dp[i][j - coins[i-1]] + 1, dp[i-1][j]);
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        
-        return dp[n][amount] == Integer.MAX_VALUE-1 ? -1 : dp[n][amount];
+
+       
+        return dp[n][amount] == Integer.MAX_VALUE ? -1 : dp[n][amount];
     }
+}
