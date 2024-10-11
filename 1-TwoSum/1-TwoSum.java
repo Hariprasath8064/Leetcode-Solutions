a721949db73780a53b2e068baf69662b38d5a749
Hariprasath M <149574480+Hariprasath8064@users.noreply.github.com>
2024-10-11 23:43:29 +0530

    Update file 10/11/2024, 11:43:25 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -2,16 +2,17 @@ class Solution {
     public int[] twoSum(int[] nums, int target) 
     {
         int n  = nums.length;
-        for(int i = 0;i<n-1;i++)
+        Map<Integer,Integer> diff = new HashMap<>();
+        for(int i = 0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            if(diff.get(nums[i]) != null)
             {
-                if(nums[i]+nums[j] == target)
-                {
-                    return new int[] {i,j};
-                }
+                return new int[] {i, diff.get(nums[i])};
             }
+            diff.put(target - nums[i],i);
         }
+        
+        
 
         throw new IllegalArgumentException("No suitable solution");
         
