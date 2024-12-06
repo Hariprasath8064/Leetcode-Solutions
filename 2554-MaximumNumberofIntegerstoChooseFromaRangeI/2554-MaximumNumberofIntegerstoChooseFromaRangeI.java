fb1a93a070f627d80e51801cf92cc855e7f01ca5
Hariprasath M <149574480+Hariprasath8064@users.noreply.github.com>
2024-12-06 22:26:54 +0530

    Create file

---
+++ 2554-MaximumNumberofIntegerstoChooseFromaRangeI/2554-MaximumNumberofIntegerstoChooseFromaRangeI.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) 
+    {
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
+        int sum = 0;
+        for(int i : banned)
+        {
+            if(i <= n)
+            {
+                set.add(i);
+            }
+        }
+
+        for(int i = 1; i <= n;i++)
+        {
+            if(!set.contains(i) && sum + i<= maxSum)
+            {
+                sum += i;
+                count++;
+            }
+            
+        }
+
+        return count;
+
+
+
+        
+        
+    }
+}
