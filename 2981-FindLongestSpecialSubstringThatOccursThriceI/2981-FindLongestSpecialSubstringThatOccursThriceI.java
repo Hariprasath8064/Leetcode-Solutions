e7d44c486fbe997d31b3fa1b02eec20798840455
Hariprasath M <149574480+Hariprasath8064@users.noreply.github.com>
2024-12-10 23:11:44 +0530

    Create file

---
+++ 2981-FindLongestSpecialSubstringThatOccursThriceI/2981-FindLongestSpecialSubstringThatOccursThriceI.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maximumLength(String s) {
+        Map<String,Integer> map = new HashMap();
+        int n = s.length();
+        int l =0;
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = i + 1;j<=n;j++)
+            {
+                String str = s.substring(i,j);
+                if(map.containsKey(str))
+                {
+                    map.put(str,map.get(str) + 1);
+                }
+                else
+                {
+                    map.put(str,1);
+                }
+            }
+        }
+
+        for(Map.Entry<String,Integer> entry : map.entrySet())
+        {
+            String str = entry.getKey();
+            int strcount = entry.getValue();
+            if(strcount >= 3)
+            {
+                Set<Character> set = new HashSet();
+                for(int j = 0;j < str.length();j++)
+                {
+                    set.add(str.charAt(j));
+                }
+
+                if (set.size() == 1)
+                {
+                    l = Math.max(l,str.length());
+                }
+            }
+        }
+        return (l == 0) ? -1: l;
+        
+    }
+}
