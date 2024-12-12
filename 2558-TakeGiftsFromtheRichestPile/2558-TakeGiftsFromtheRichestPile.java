b7fa848093a73bab08b72e1400e80741f866db8d
Hariprasath M <149574480+Hariprasath8064@users.noreply.github.com>
2024-12-12 22:02:03 +0530

    Create file

---
+++ 2558-TakeGiftsFromtheRichestPile/2558-TakeGiftsFromtheRichestPile.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        Arrays.sort(gifts);
+        long sum = 0;
+    
+        for(int i = 0;i<k;i++)
+        {
+            int index = gifts.length - 1;
+            gifts[index] = (int) Math.floor(Math.sqrt(gifts[index]));
+            Arrays.sort(gifts);
+
+
+        }
+        for(int gift:gifts)
+        {
+            sum += gift;
+        }
+        return sum;
+    }
+}
