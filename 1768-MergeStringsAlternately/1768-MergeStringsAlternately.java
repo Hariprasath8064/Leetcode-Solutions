a3696c45e4e7eba446776b5b6c18d4cf849e654d
Hariprasath M <149574480+Hariprasath8064@users.noreply.github.com>
2025-01-14 21:07:40 +0530

    Create file

---
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        char[] result = new char[n + m];
+
+        int i = 0 , j = 0, k = 0;
+        while ( i < n || j < m){
+            if (i  < n){
+                result[k++] = word1.charAt(i++);
+            }
+            if(j < m){
+                result[k++] = word2.charAt(j++);
+            }
+        }
+
+        return new String(result);
+        
+        
+    }
+}
