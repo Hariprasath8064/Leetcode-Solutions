71f6efb2bef3bbbe74a61c90fdf3048a989192b9
Hariprasath M <hariprasathm777@gmail.com>
2025-03-03 18:32:24 +0530

    Create file

---
+++ 2222-NumberofWaystoSelectBuildings/2222-NumberofWaystoSelectBuildings.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long numberOfWays(String s) {
+        int n = s.length();
+        long lz = 0 ;
+        long lo = 0;
+        long rz = 0;
+        long ro = 0;
+        long count = 0;
+
+        for ( char c : s.toCharArray()){
+            if(c == '0'){
+                rz++;
+            }
+            else
+            {
+                ro++;
+            }
+        }
+
+        for(char c : s.toCharArray()){
+            if(c == '0'){
+                rz--;
+                count += lo * ro;
+                lz++;
+            }
+            else{
+                ro--;
+                count += lz * rz;
+                lo++;
+            }
+        }
+
+        return count;
+
+        
+    }
+}
