87850b76e1cfee0fd83821c9910883aa8eb05100
Hariprasath M <hariprasathm777@gmail.com>
2025-03-12 19:33:43 +0530

    File updated at 3/12/2025, 7:33:42 PM

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,17 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        if(n == 1){
-            return nums[0];
+        int x = 0;
+        for(int num : nums){
+            x ^= num;
         }
-
-        for(int i = 0;i<n - 1;i+=2){
-            if(nums[i] != nums[i+1]){
-                return nums[i];
-            }
-        }
-        return nums[n - 1];
+        return x;
         
     }
 }
