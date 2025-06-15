4fbbbc41113e9ab5c9e4a148cefd5312a8e6d5d9
Hariprasath M <hariprasathm777@gmail.com>
2025-06-15 17:28:28 +0530

    Create 1849-MaximumAbsoluteSumOfAnySubarray

---
+++ 1849-MaximumAbsoluteSumOfAnySubarray/1849-MaximumAbsoluteSumOfAnySubarray.java
@@ -0,0 +1,19 @@
+// Last updated: 6/15/2025, 5:28:28 PM
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        int currentmax = 0;
+        int currentmin = 0;
+        int maxsum = 0;
+        int minsum = 0;
+
+        for(int num:nums)
+        {
+            currentmax = Math.max(num , currentmax + num);
+            currentmin = Math.min(num , currentmin + num);
+            maxsum = Math.max(maxsum,currentmax);
+            minsum = Math.min(minsum,currentmin);
+        }
+        return Math.max(Math.abs(maxsum),Math.abs(minsum));
+        
+    }
+}
