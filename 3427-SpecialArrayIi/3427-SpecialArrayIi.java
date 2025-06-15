748a971c1010b51a570c33d25169c9ad41ebfc66
Hariprasath M <hariprasathm777@gmail.com>
2025-06-15 17:27:55 +0530

    Create 3427-SpecialArrayIi

---
+++ 3427-SpecialArrayIi/3427-SpecialArrayIi.java
@@ -0,0 +1,30 @@
+// Last updated: 6/15/2025, 5:27:55 PM
+class Solution {
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int m = queries.length;
+        int[] prefix = new int[n];
+        boolean[] ans = new boolean[m];
+        prefix[0] = 0;
+        for(int i = 1;i<n;i++)
+        {
+            if(nums[i] % 2 == nums[i -1]%2)
+            {
+                prefix[i] = prefix[i - 1]+1;
+            }
+            else
+            {
+                prefix[i] = prefix[i - 1];
+            }
+        }
+        for(int i = 0;i<m;i++)
+        {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            ans[i] = (prefix[s] == prefix[e]);
+        }
+        return ans;
+        
+
+    }
+}
