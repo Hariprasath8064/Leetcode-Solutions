6788feae82ddeff71d0cacc4a653df340aaff588
Hariprasath M <hariprasathm777@gmail.com>
2025-06-17 21:50:35 +0530

    File updated at 6/17/2025, 9:50:35 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,30 +1,31 @@
+// Last updated: 6/17/2025, 9:50:35 PM
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())
-        {
+        if (s.length() != t.length()) {
             return false;
         }
-        HashMap<Character, Integer> count = new HashMap<>();
-        for(char c : s.toCharArray())
-        {
-            count.put(c,count.getOrDefault(c,0) + 1);
+
+        HashMap<Character, Integer> mapS = new HashMap<>();
+        HashMap<Character, Integer> mapT = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            mapS.put(c, mapS.getOrDefault(c, 0) + 1);
         }
 
-        for(char c:t.toCharArray()){
-            if(!count.containsKey(c))
-            {
+        for (char c : t.toCharArray()) {
+            if (!mapS.containsKey(c)) {
                 return false;
             }
-            count.put(c,count.get(c) - 1);
 
-            if(count.get(c) == 0)
-            {
-                count.remove(c);
+            int newCount = mapT.getOrDefault(c, 0) + 1;
+
+            if (newCount > mapS.get(c)) {
+                return false;
             }
+
+            mapT.put(c, newCount);
         }
 
-        return count.isEmpty();
-
-        
+        return true; 
     }
 }
