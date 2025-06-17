128bb089583ca12592d27e1aaff8233dfc3bed45
Hariprasath M <hariprasathm777@gmail.com>
2025-06-17 21:04:10 +0530

    File updated at 6/17/2025, 9:04:10 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,20 +1,17 @@
+// Last updated: 6/17/2025, 9:04:10 PM
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int n = nums.length;
-        int count = 0;
-        int max ;
-        for(int num : nums)
-        {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
             map.put(num,map.getOrDefault(num,0) + 1);
-            if(map.get(num) > 1)
-            {
+        }
+        for(int count : map.values()){
+            if(count > 1){
                 return true;
             }
-
         }
-        return false;
 
+        return false;
         
     }
 }
