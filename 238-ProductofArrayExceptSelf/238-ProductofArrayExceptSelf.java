90f038cddc69308baa0769be62355b20968e5fcb
Hariprasath M <hariprasathm777@gmail.com>
2025-06-18 23:38:59 +0530

    File updated at 6/18/2025, 11:38:59 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,14 +1,35 @@
+// Last updated: 6/18/2025, 11:38:59 PM
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int[] ans = new int[nums.length];
-        Arrays.fill(ans, 1);
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i != j) {
-                    ans[i] *= nums[j];
+    public boolean isValidSudoku(char[][] board) {
+        Set<Character>[] rows = new HashSet[9];
+        Set<Character>[] cols = new HashSet[9];
+        Set<Character>[] boxes = new HashSet[9];
+
+        for(int i=0;i<9;i++){
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+
+                if (c == '.') {
+                    continue; 
                 }
+
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                if (rows[i].contains(c) || cols[j].contains(c) || boxes[boxIndex].contains(c)) {
+                    return false;
+                }
+
+                rows[i].add(c);
+                cols[j].add(c);
+                boxes[boxIndex].add(c);
             }
         }
-        return ans;
+
+        return true;
     }
 }
