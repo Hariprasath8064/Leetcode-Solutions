466a0a350ab1df626371674f07da9a6f26881648
Hariprasath M <hariprasathm777@gmail.com>
2025-06-18 23:56:16 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -0,0 +1,26 @@
+// Last updated: 6/18/2025, 11:56:17 PM
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int count = 1;
+        int highest = 1;
+
+        for(int i= 1; i < nums.length ; i++){
+            if(nums[i] == nums[i - 1]){
+                continue;
+            }
+            if(nums[i] == nums[i - 1] + 1){
+                count ++;
+            }
+            else{
+                count = 1;
+            }
+            highest = Math.max(highest,count);
+        }
+        return highest;
+        
+    }
+}
