2f910bc844d0151c0ecff737fcf9859168952648
Hariprasath M <hariprasathm777@gmail.com>
2025-06-21 00:18:29 +0530

    File updated at 6/21/2025, 12:18:29 AM

--- 704-BinarySearch/704-BinarySearch.java
+++ 704-BinarySearch/704-BinarySearch.java
@@ -1,25 +1,35 @@
-// Last updated: 6/20/2025, 11:28:31 PM
+// Last updated: 6/21/2025, 12:18:29 AM
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int l = 0;
-        int r = m*n - 1;
-        while(l <= r){
-            int mid = l + (r -l)/2;
-            int row = mid / n;
-            int col = mid % n;
-            int val = matrix[row][col];
-            if(val == target){
-                return true;
-            }
-            else if(val < target){
-                l = mid + 1;
+    public int minEatingSpeed(int[] piles, int h) {
+        int maxk = 0;
+        for(int num:piles){
+            maxk = Math.max(maxk , num);
+        }
+
+        int left = 1;
+        int right = maxk;
+        int res = maxk;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            long hours = calh(piles,mid);
+            if(hours <= h){
+                res = mid;
+                right = mid -1;
             }
             else{
-                r = mid - 1;
+                left = mid + 1;
             }
         }
-        return false;
+        return res;
+        
+    }
+
+    private long calh(int [] piles,int mid){
+        long h = 0;
+        for(int num: piles){
+            h += (num + (long)mid - 1) / mid;
+        }
+        return h;
+
     }
 }
