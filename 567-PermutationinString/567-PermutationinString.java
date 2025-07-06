23628036c7374f91489d2345d1ce63a2b855d486
Hariprasath M <hariprasathm777@gmail.com>
2025-07-06 21:58:56 +0530

    Create 567-PermutationinString

---
+++ 567-PermutationinString/567-PermutationinString.java
@@ -0,0 +1,30 @@
+// Last updated: 7/6/2025, 9:58:56 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+
+        root.right = left;
+        root.left = right;
+
+        return root;
+    }
+}
