925d0722c33dbbdbdde7ce56a18cd17c655e1c84
Hariprasath M <hariprasathm777@gmail.com>
2025-07-07 12:45:19 +0530

    Create 274-H-Index

---
+++ 274-H-Index/274-H-Index.java
@@ -0,0 +1,12 @@
+// Last updated: 7/7/2025, 12:45:19 PM
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        for(int i = 0 ;i < citations.length;i++){
+            if(citations[i] >= citations.length - i){
+                return citations.length - i;
+            }
+        }
+        return 0;
+    }
+}
