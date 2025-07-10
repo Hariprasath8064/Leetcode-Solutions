24646af4ed9f84f62b291e114defe1edf2c1baf6
Hariprasath M <hariprasathm777@gmail.com>
2025-07-10 23:23:48 +0530

    File updated at 7/10/2025, 11:23:48 PM

--- 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.java
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.java
@@ -1,28 +1,36 @@
-// Last updated: 7/10/2025, 9:26:21 PM
+// Last updated: 7/10/2025, 11:23:48 PM
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode current = root ;
-        while(current != null){
-            if(p.val > current.val && q.val > current.val){
-                current = current.right;
-            }
-            else if(p.val < current.val && q.val < current.val){
-                current = current.left;
-            }
-            else{
-                return current;
-            }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(root,0,res);
+        return res;
+    }
+    public void dfs(TreeNode p , int count , List<List<Integer>> res){
+        if(p == null){
+            return;
         }
-        return current;
+        if(res.size() == count){
+            res.add(new ArrayList<>());
+        }
+        res.get(count).add(p.val);
+
+        dfs(p.left,count+1,res);
+        dfs(p.right,count+1,res);
+
     }
 }
