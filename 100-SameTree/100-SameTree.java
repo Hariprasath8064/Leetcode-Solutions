395a9f4da841d07f77830588f7bd271e380c1c3d
Hariprasath M <hariprasathm777@gmail.com>
2025-09-06 17:57:00 +0530

    File updated at 9/6/2025, 5:56:59 PM

--- 100-SameTree/100-SameTree.java
+++ 100-SameTree/100-SameTree.java
@@ -1,40 +1,14 @@
-// Last updated: 7/10/2025, 7:52:52 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Last updated: 9/6/2025, 5:56:59 PM
 class Solution {
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(subRoot == null){
-            return true;
-        }
-        if(root == null){
-            return false;
-        }
-        if(sametree(root,subRoot) == true){
-            return true;
-        }
-        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+    public boolean canJump(int[] nums) {
+        int req = 1;
+        int start = nums.length - 1;
 
-    }
-    public boolean sametree(TreeNode p , TreeNode q){
-        if (p == null && q == null){
-            return true;
+        for(int i = nums.length - 2;i >= 0;i--){
+            if(i + nums[i] >= start){
+                start = i;
+            }
         }
-        if(p == null || q == null || p.val != q.val){
-            return false;
-        }
-        return sametree(p.left,q.left) && sametree(p.right,q.right);
+        return start == 0;
     }
 }
