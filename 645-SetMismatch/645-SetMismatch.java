d09b166dd1db14642e2ec0902d94d51cc8b7a1c2
Hariprasath M <hariprasathm777@gmail.com>
2025-12-07 23:18:12 +0530

    File updated at 12/7/2025, 11:18:12 PM

--- 645-SetMismatch/645-SetMismatch.java
+++ 645-SetMismatch/645-SetMismatch.java
@@ -1,20 +1,22 @@
-// Last updated: 12/6/2025, 11:00:16 PM
+// Last updated: 12/7/2025, 11:18:12 PM
 1class Solution {
-2    public int[] findErrorNums(int[] nums) {
+2    public int[] smallerNumbersThanCurrent(int[] nums) {
 3        int n = nums.length;
-4        int ans[] = new int[2];
-5        ArrayList<Integer> list = new ArrayList<>();
-6        for(int i = 1;i<=n;i++){
-7            list.add(i);
-8        }
-9        Arrays.sort(nums);
-10        for(int i = 0;i < n;i++){
-11            if(i < n-1 && nums[i] == nums[i+1]){
-12                ans[0] = nums[i];
+4        int[] ans = new int[n];
+5
+6        for (int i = 0; i < n; i++) {
+7            int count = 0;
+8
+9            for (int j = 0; j < n; j++) {
+10                if (nums[j] < nums[i]) {
+11                    count++;
+12                }
 13            }
-14            list.remove(Integer.valueOf(nums[i]));
-15        }
-16        ans[1] = list.get(0);
-17        return ans;
-18    }
-19}
+14
+15            ans[i] = count;
+16        }
+17
+18        return ans;
+19    }
+20}
+21
