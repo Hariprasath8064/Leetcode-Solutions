19fb11c34eed53909bbcab90b91e57cdf1e31c78
Hariprasath M <hariprasathm777@gmail.com>
2025-12-11 15:08:26 +0530

    File updated at 12/11/2025, 3:08:26 PM

--- 1441-BuildanArrayWithStackOperations/1441-BuildanArrayWithStackOperations.java
+++ 1441-BuildanArrayWithStackOperations/1441-BuildanArrayWithStackOperations.java
@@ -1,17 +1,36 @@
-// Last updated: 12/10/2025, 3:53:38 PM
+// Last updated: 12/11/2025, 3:08:26 PM
 1class Solution {
-2    public List<String> buildArray(int[] target, int n) {
-3        List<String> ans = new ArrayList<>();
-4        int initial = 1;
-5        for( int i : target){
-6            while(initial < i){
-7                ans.add("Push");
-8                ans.add("Pop");
-9                initial++;
-10            }
-11            ans.add("Push");
-12            initial++;
-13        }
-14        return ans;
-15    }
-16}
+2    public int evalRPN(String[] tokens) {
+3        Stack<Integer> stack = new Stack<>();
+4
+5        for (String token : tokens) {
+6            if (!token.equals("+") && !token.equals("-") && 
+7                !token.equals("*") && !token.equals("/")) {
+8                
+9                stack.push(Integer.parseInt(token));
+10            } 
+11            else { 
+12                int b = stack.pop();
+13                int a = stack.pop();
+14
+15                switch (token) {
+16                    case "+":
+17                        stack.push(a + b);
+18                        break;
+19                    case "-":
+20                        stack.push(a - b);
+21                        break;
+22                    case "*":
+23                        stack.push(a * b);
+24                        break;
+25                    case "/":
+26                        stack.push(a / b);
+27                        break;
+28                }
+29            }
+30        }
+31
+32        return stack.pop();
+33    }
+34}
+35
